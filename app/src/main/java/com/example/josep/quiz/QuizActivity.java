b2b6b9a4cea8df8911608d75411d7286535eb914
alif3595 <alif.leong.14@ucl.ac.uk>
2016-12-07 17:51:10 +0000

    Deleted cheat and skip buttons. Added Question class.

--- app/src/main/java/com/example/josep/quiz/QuizActivity.java
+++ app/src/main/java/com/example/josep/quiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.josep.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView; //imports TextView class
 import android.view.View;     //imports View class
 import android.widget.Button; //imports Button class
 import android.widget.Toast;  //imports Toast class
@@ -11,11 +12,26 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_text, true),
+            new Question(R.string.question_1, true),
+            new Question(R.string.question_2, false),
+            new Question(R.string.question_3, true),
+            new Question(R.string.question_4, false),
+    };
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        //sets question text
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
 
         //sets a Listener on the true button and creates Toast for answers
         mTrueButton = (Button) findViewById(R.id.true_button);
