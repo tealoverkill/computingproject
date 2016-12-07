bae37c4525711d342075b3b858413ca9e419b563
alif3595 <alif.leong.14@ucl.ac.uk>
2016-12-07 19:09:10 +0000

    Added Next button and answers display correctly when True or False buttons are pressed.

--- app/src/main/java/com/example/josep/quiz/QuizActivity.java
+++ app/src/main/java/com/example/josep/quiz/QuizActivity.java
@@ -18,15 +18,24 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_text, true),
             new Question(R.string.question_1, true),
             new Question(R.string.question_2, false),
-            new Question(R.string.question_3, true),
+            new Question(R.string.question_3, false),
             new Question(R.string.question_4, false),
     };
     private int mCurrentIndex = 0;
+    private boolean answer = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+    //method for loading next question
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+
 
         //sets question text
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -35,27 +44,57 @@ public class QuizActivity extends AppCompatActivity {
 
         //sets a Listener on the true button and creates Toast for answers
         mTrueButton = (Button) findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+            mTrueButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    if (answer) {
+                        Toast.makeText(QuizActivity.this,
+                                R.string.correct_toast,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(QuizActivity.this,
+                                R.string.incorrect_toast,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
 
 
         //sets a Listener on the false button and creates Toast for answers
         mFalseButton = (Button) findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            mFalseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    if (answer) {
+                        Toast.makeText(QuizActivity.this,
+                                R.string.incorrect_toast,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    else    {
+                        Toast.makeText(QuizActivity.this,
+                                R.string.correct_toast,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+        //sets a Listener on the next button and increases mCurrentIndex every time next button is clicked
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                answer = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                updateQuestion();
             }
         });
+
+        updateQuestion();
     }
 }
