088bf2ca7fc8749cb96c75f7b2226c8552a5d52f
alif3595 <alif.leong.14@ucl.ac.uk>
2016-12-08 12:39:32 +0000

    Added CheatActivity. Cheat functionality included for 1 Player game.

--- app/src/main/java/com/example/josep/quiz/QuizActivity.java
+++ app/src/main/java/com/example/josep/quiz/QuizActivity.java
@@ -1,5 +1,7 @@
 package com.example.josep.quiz;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView; //imports TextView class
@@ -10,9 +12,12 @@ import android.widget.Toast;  //imports Toast class
 //This activity shows each question of the quiz
 public class QuizActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_CHEAT = 0;
+
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_text, true),
@@ -22,7 +27,7 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_4, false),
     };
     private int mCurrentIndex = 0;
-    private boolean answer = mQuestionBank[mCurrentIndex].isAnswerTrue();
+    private boolean mIsCheater;
 
     //method for loading next question
     private void updateQuestion() {
@@ -30,12 +35,41 @@ public class QuizActivity extends AppCompatActivity {
         mQuestionTextView.setText(question);
     }
 
+    //method for checking answer
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if (mIsCheater) {
+            messageResId = R.string.judgment_toast;
+        } else {
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-
+        //sets a Listener on the cheat button
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start CheatActivity
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent i = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+                startActivityForResult(i, REQUEST_CODE_CHEAT);
+            }
+        });
 
         //sets question text
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -44,44 +78,24 @@ public class QuizActivity extends AppCompatActivity {
 
         //sets a Listener on the true button and creates Toast for answers
         mTrueButton = (Button) findViewById(R.id.true_button);
-            mTrueButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v)
-                {
-                    if (answer) {
-                        Toast.makeText(QuizActivity.this,
-                                R.string.correct_toast,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        Toast.makeText(QuizActivity.this,
-                                R.string.incorrect_toast,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
 
 
 
 
         //sets a Listener on the false button and creates Toast for answers
         mFalseButton = (Button) findViewById(R.id.false_button);
-            mFalseButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v)
-                {
-                    if (answer) {
-                        Toast.makeText(QuizActivity.this,
-                                R.string.incorrect_toast,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                    else    {
-                        Toast.makeText(QuizActivity.this,
-                                R.string.correct_toast,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
 
 
         //sets a Listener on the next button and increases mCurrentIndex every time next button is clicked
@@ -90,11 +104,25 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                answer = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                mIsCheater = false;
                 updateQuestion();
             }
         });
 
         updateQuestion();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
+    }
+
 }
